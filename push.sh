f01b83d8dc1a1a915a46730e951263490c7e3c2d
554108 <git>
2023-10-25 09:27:48 +0800

    自动推送

--- push.sh
+++ push.sh
@@ -1,9 +1,8 @@
-git config --global user.name 'yangjh-xbmu'
-git config --global user.email 'yangjh@yeah.net'
+git config --global user.name '554108'
+git config --global user.email '2023269569@qq.com'
 git config --global http.sslverify false
 git add .
-git commit -m $1
+git commit -m '×Ô¶¯ÍÆËÍ'
 git push
-git config --global user.name '554108'
-git config --global user.email '3023269569@qq.com
+
 '
